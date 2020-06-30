f1d876da003cde9f066da4212265723638eba2b4
haitho <haitho263@gmail.com>
2020-06-30 13:20:43 +0700

    upcode server
    upcode server

--- app.py
+++ app.py
@@ -1,46 +1,117 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 import threading
 import time
 
-from articles import getNewsFromId, getLastId, saveArticles
+from articles import getArticlesByCategory, getArticleById, getHotNews, getNewArticles, searchArticles, crawlHotNews, crawlAll
+from music import getMusicByCountry, crawAndSaveDB
 from constance import constances
 app = Flask(__name__)
 api = Api(app)
 
 
-class Articles(Resource):
-    def get(self, id):
+class Article(Resource):
+    parser = reqparse.RequestParser()
+
+    def get(self):
         items = []
-        if (int(id) == 0):
-            items = getNewsFromId(getLastId())
-        else:
-            items = getNewsFromId(int(id))
+        id = int(request.args.get('id'))
+        items = getArticleById(id)
+        # if (int(id) == 0):
+        #     items = getNewsFromId(getLastId())
+        # else:
+        #     items = getNewsFromId(int(id))
         if items is None:
             return {'message': "Can't get articles"}, 400
         return items, 200
 
-api.add_resource(Articles, '/article/<string:id>')
+
+class Articles(Resource):
+    parser = reqparse.RequestParser()
+
+    def get(self):
+        items = []
+        offset = int(request.args.get('offset'))*constances.ARTICLES_PER_LOAD
+        cat = request.args.get('cat')
+        print(cat)
+        items = getArticlesByCategory(cat, offset)
+
+        if items is None:
+            return {'message': "Can't get articles"}, 400
+        return items, 200
 
 
-def crawlData():
+class Articles2(Resource):
+    parser = reqparse.RequestParser()
+
+    def get(self, tag):
+        items = []
+        offset = int(request.args.get('offset'))*constances.ARTICLES_PER_LOAD
+        if(tag == 'hot'):
+            items = getHotNews(offset)
+        elif (tag == 'new'):
+            items = getNewArticles(offset)
+
+        if items is None:
+            return {'message': "Can't get articles"}, 400
+        return items, 200
+
+class SearchArticle(Resource):
+    parser = reqparse.RequestParser()
+
+    def get(self):
+        items = []
+        info = request.args.get('info')
+        items = searchArticles(info,0)
+        if items is None:
+            return {'message': "Can't get articles"}, 400
+        return items, 200
+
+class Music(Resource):
+    def get(self, country):
+        items = []
+        items = getMusicByCountry(country)
+
+        if items is None:
+            return {'message': "Can't get songs"}, 400
+        return items, 200
+
+
+api.add_resource(Article, '/article/')
+api.add_resource(Articles, '/articles/')
+api.add_resource(Articles2, '/articles/<string:tag>/')
+api.add_resource(SearchArticle, '/articles/search/')
+api.add_resource(Music, '/music/<string:country>')
+
+def crawlArticle():
     while True:
-        saveArticles()
+        crawlHotNews()
+        crawlAll(1)
+        print("News Crawled")
         time.sleep(constances.TIME_BETWEEN_CRAWL)
-        print("Crawled")
 
 
-@app.route('/run')
-def craw():
+@app.route('/article/run')
+def crawArticleThread():
     threading.Thread(
-        target=crawlData,
+        target=crawlArticle,
     ).start()
-    return "Crawling"
+    return "Crawling articles"
 
-@app.route('/hello')
-def Hello():
-    return "Hello"
+# def crawlMusic():
+#     while True:
+#         print("Music Crawled")
+#         crawAndSaveDB()
+#         time.sleep(43200)
+
+
+# @app.route('/music/run')
+# def crawMusicThread():
+#     threading.Thread(
+#         target=crawlMusic,
+#     ).start()
+#     return "Crawling music"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, threaded=True)
--- constance.py
+++ constance.py
@@ -1,14 +1,20 @@
 class constances:
-    # HOST = "localhost"
-    # USERNAME = "root"
+    HOST = "localhost"
+    USERNAME = "root"
+    PASSWORD = "Aa@123456"
+    DATABASE = "datatrends"
+
+    # ARTICLES_PER_LOAD = 1*10
+    # HOST = "eu-cdbr-west-03.cleardb.net"
+    # USERNAME = "be7e95159ef4b6"
+    # PASSWORD = "6ae3fcbb"
+    # DATABASE = "heroku_11541cfc29f14b7"
+    #
+    # HOST = "datatrends.mysql.database.azure.com"
+    # USERNAME = "admin294@datatrends"
     # PASSWORD = "Aa@123456"
     # DATABASE = "datatrends"
-    # ARTICLES_PER_LOAD = 14
-    
+
+    ARTICLES_PER_LOAD = 16
     TIME_BETWEEN_CRAWL = 10800
     DELETE_AFTER = 3
-    ARTICLES_PER_LOAD = 1*10
-    HOST = "eu-cdbr-west-03.cleardb.net"
-    USERNAME = "be7e95159ef4b6"
-    PASSWORD = "6ae3fcbb"
-    DATABASE = "heroku_11541cfc29f14b7"
